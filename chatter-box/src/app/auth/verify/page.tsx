f51eaaee08ce5db0bda14b114300540e580e7890
Argel Hernandez <argel6767@gmail.com>
2025-07-13 16:02:59 -0500

    add verify

--- chatter-box/src/app/auth/verify/page.tsx
+++ chatter-box/src/app/auth/verify/page.tsx
@@ -51,7 +51,7 @@ const Verify = async ({searchParams}:VerifyProps) => {
     return (<div
         className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 flex items-center justify-center p-4">
         <div className="w-full max-w-md">
-            <BackButton/>
+            <BackButton path={"/auth"}/>
             <AnnouncementMessage title={getTitle()} message={response.message}/>
         </div>
     </div>)
